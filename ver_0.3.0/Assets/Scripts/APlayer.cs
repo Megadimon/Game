f1cf5877e906e7d14c35484715b7717801c84a1b
Megadimon <md.65@mail.ru>
2024-12-23 00:12:21 +0300

    APlaeyr_change(set,get)

--- ver_0.3.0/Assets/Scripts/APlayer.cs
+++ ver_0.3.0/Assets/Scripts/APlayer.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 using UnityEngine.UI;
 public abstract class APlayer : MonoBehaviour
 {
-    private int healthOfPlayer { get; set; }
-    private float speedOfPlayer;
-    private float fuelOfPlayer;
-    public Slider hpBar;
-    public Slider fuelBar;
-    public bool isYouTurn;
+    private int _healthOfPlayer;
+    private float _speedOfPlayer;
+    private float _fuelOfPlayer;
+    public Slider _hpBar;
+    public Slider _fuelBar;
+    public bool _isYouTurn;
+
+    public abstract int HealthSetup { get; set; }
+    public abstract int SpeedSetup { get; set; }
+    public abstract int FuelOfPlayerSetup { get; set; }
 
     public abstract void PlayerMove();
     public abstract void PlayerShot();
